f5037a657da206b6d224a08e1085c51050a42ab3
Lam Son Hai <hhoa3267@Lams-MacBook-Pro.local>
2020-07-20 23:35:50 -0700

    add route

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+
+import App from './App';
+import Register from './Components/Register/Register';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <Switch>
+      <Route path='/Register' component={Register} />
+      <Route path='/' component={App} />
+    </Switch>
+  </Router>,
   document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
